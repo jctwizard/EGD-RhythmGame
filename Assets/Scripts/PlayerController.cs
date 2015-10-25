8d66619c1f37c5c3c143465c2c065743171e48dc
James Wood <jamie_wood@ymail.com>
2015-10-25 15:02:19 +0000

    Add player animations and desert assets
    Add the four player animations and a player controller with debug keys
    for each animation. Add desert assets.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	enum State { Attack, Defence, Idle, Run };
+	Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Debug change animaton
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			Debug.Log ("pressed 1");
+			animator.SetInteger("State", (int)State.Attack);
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			animator.SetInteger("State" , (int)State.Defence);
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			animator.SetInteger("State" , (int)State.Idle);
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			animator.SetInteger("State" , (int)State.Run);
+		}
+	}
+}
